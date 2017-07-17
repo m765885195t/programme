48229e1590069c9e94317c0aa20d0c5872fabad5
motian <motian@xiyoulinux.org>
2017-07-17 21:55:21 +0800

    sda

---
+++ code/c/1.c
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: 1.c
+	> Author: motian
+	> Mail: motian@xiyoulinux.org
+	> Created Time: 2017年07月17日 星期一 21时54分08秒
+ ************************************************************************/
+
+#include<stdio.h>
+#include<stdlib.h>
+int main(int argc,char *argv[])
+{
+
+	return 0;
+}
